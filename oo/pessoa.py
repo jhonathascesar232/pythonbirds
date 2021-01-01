2e9cc724470cef880f232676bbb3a71285e0e972
Jhonathas César <jhonathascesar232@gmail.com>
2021-01-01 17:55:25 -0300

    Herança com o metodo super

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -6,18 +6,22 @@ class Pessoa:
         self.idade = idade
         self.filhos = list(filhos)
     def comprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá {self.nome}'
     @staticmethod
     def metodo_estatico():
         return 42
     @classmethod
     def nomes_e_attr_de_classe(cls):
         return f'{cls} olhos {cls.olhos}'
+class Homen(Pessoa):    #herança
+    def comprimentar(self):
+        comprimentar_da_classe = super().comprimentar()
+        return f'{comprimentar_da_classe}. Aperto de mãos.'
 
 
 if __name__ == '__main__':
     j = Pessoa(nome = 'Jhonathas', idade = 23)
-    c = Pessoa(j, nome = 'César', idade = 51)
+    c = Homen(j, nome = 'César', idade = 51)
     
     print(j.nome)
     print(c.nome)
@@ -26,3 +30,5 @@ if __name__ == '__main__':
 
     print(Pessoa.metodo_estatico(), j.metodo_estatico())
     print(Pessoa.nomes_e_attr_de_classe(), j.nomes_e_attr_de_classe())
+
+    print(c.comprimentar())
